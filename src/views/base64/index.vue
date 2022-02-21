8a15170048ae89a6a4254e4e0fc704aa511fb7a8
wataly <wataly.sh@gmail.com>
2022-02-21 23:52:30 +0800

    init

---
+++ src/views/base64/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app-container">
+    <div>
+      <el-input v-model="originText" type="textarea" />
+    </div>
+    <div>
+      <el-button type="primary" @click="encodeText">Base64加密</el-button>
+      <el-button type="primary" @click="decodeText">Base64解密</el-button>
+      <el-button type="primary" @click="clearText">清空</el-button>
+    </div>
+    <div>
+      <el-input v-model="transferText" type="textarea" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      originText: '',
+      transferText: ''
+    }
+  },
+  methods: {
+    // eslint-disable-next-line vue/no-dupe-keys
+    encodeText() {
+      this.transferText = Buffer.from(this.originText, 'utf-8').toString('base64')
+    },
+    // eslint-disable-next-line vue/no-dupe-keys
+    decodeText() {
+      this.originText = Buffer.from(this.transferText, 'base64').toString('utf-8')
+    },
+    clearText() {
+      this.originText = ''
+      this.transferText = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
+
---
+++ src/views/url/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="app-container">
+    <div>
+      <el-input v-model="originText" type="textarea" />
+    </div>
+    <div>
+      <el-button type="primary" @click="encodeText">Url编码</el-button>
+      <el-button type="primary" @click="decodeText">Url解码</el-button>
+      <el-button type="primary" @click="clearText">清空</el-button>
+    </div>
+    <div>
+      <el-input v-model="transferText" type="textarea" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      originText: '',
+      transferText: ''
+    }
+  },
+  methods: {
+    encodeText() {
+      this.transferText = encodeURI(this.originText)
+    },
+    decodeText() {
+      this.originText = decodeURI(this.transferText)
+    },
+    clearText() {
+      this.originText = ''
+      this.transferText = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
+

ab8c233a51bcff017638c2b74b3ac524ebd42900
wataly <wataly.sh@gmail.com>
2022-02-23 23:38:42 +0800

    remove unused codes

--- src/router/index.js
+++ src/router/index.js
@@ -32,12 +32,6 @@ import Layout from '@/layout'
  */
 export const constantRoutes = [
   {
-    path: '/login',
-    component: () => import('@/views/login/index'),
-    hidden: true
-  },
-
-  {
     path: '/404',
     component: () => import('@/views/404'),
     hidden: true
@@ -53,41 +47,6 @@ export const constantRoutes = [
       component: () => import('@/views/dashboard/index'),
       meta: { title: 'Dashboard', icon: 'dashboard' }
     }]
-  },
-
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'el-icon-s-help' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
-  },
-
-  {
-    path: '/form',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
-      }
-    ]
   },
   {
     path: '/base64',
@@ -108,7 +67,7 @@ export const constantRoutes = [
     children: [
       {
         path: 'index',
-        name: 'Url编码',
+        name: 'Url',
         component: () => import('@/views/url/index'),
         meta: { title: 'Url编码', icon: 'form' }
       }
@@ -120,9 +79,9 @@ export const constantRoutes = [
     children: [
       {
         path: 'index',
-        name: 'JsonViewer',
+        name: 'JSON',
         component: () => import('@/views/json/index'),
-        meta: { title: 'JsonViewer', icon: 'form' }
+        meta: { title: 'JsonEditor', icon: 'form' }
       }
     ]
   },
@@ -132,79 +91,19 @@ export const constantRoutes = [
     children: [
       {
         path: 'index',
-        name: '时间戳工具',
+        name: 'Time',
         component: () => import('@/views/time/index'),
         meta: { title: '时间戳工具', icon: 'form' }
       }
     ]
   },
-
-  {
-    path: '/nested',
-    component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {
-      title: 'Nested',
-      icon: 'nested'
-    },
-    children: [
-      {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
-      },
-      {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        name: 'Menu2',
-        meta: { title: 'menu2' }
-      }
-    ]
-  },
-
   {
     path: 'external-link',
     component: Layout,
     children: [
       {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
+        path: 'https://xhdoc.com',
+        meta: { title: 'Blog', icon: 'link' }
       }
     ]
   },
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <ul>
+      <li><router-link :to="{name:'Base64'}">Base64加解密</router-link></li>
+      <li><router-link :to="{name:'Url'}">Url Encode/Decode</router-link></li>
+      <li><router-link :to="{name:'JSON'}">JSON工具</router-link></li>
+      <li><router-link :to="{name:'Time'}">时间戳工具</router-link></li>
+    </ul>
   </div>
 </template>
 
@@ -26,5 +31,8 @@ export default {
     font-size: 30px;
     line-height: 46px;
   }
+}
+ul li{
+  margin: 10px auto;
 }
 </style>

ce8985bc1e9e38805cd337fda450e4ca401e1312
wataly <wataly.sh@gmail.com>
2022-03-03 22:19:15 +0800

    update dashboard

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,9 +2,15 @@
   <div class="dashboard-container">
     <ul>
       <li><router-link :to="{name:'Base64'}">Base64加解密</router-link></li>
+      <li>
+        <router-link :to="{name:'MD5'}">Md5加密</router-link>
+      </li>
       <li><router-link :to="{name:'Url'}">Url Encode/Decode</router-link></li>
       <li><router-link :to="{name:'JSON'}">JSON工具</router-link></li>
       <li><router-link :to="{name:'Time'}">时间戳工具</router-link></li>
+      <li>
+        <router-link :to="{name:'JWT'}">Jwt解密</router-link>
+      </li>
     </ul>
   </div>
 </template>
@@ -32,7 +38,8 @@ export default {
     line-height: 46px;
   }
 }
-ul li{
-  margin: 10px auto;
+ul li {
+  margin: 20px auto;
+  color: blue;
 }
 </style>
